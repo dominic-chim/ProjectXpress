d08920d9034ab94bc0798a66288b607af5901bee
rxj01u <rxj01u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-01-17 11:44:10 +0000

    Added Main Container
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@75 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/view/staff/StaffPanel.java
+++ src/view/staff/StaffPanel.java
@@ -4,4 +4,23 @@ import javax.swing.JPanel;
 
 public class StaffPanel extends JPanel {
 
+	// Left panel - Staff Summary
+	private StaffSummary staffSummary = new StaffSummary();
+	
+	// Right tabbed pane
+	private JTabbedPane rightTabPane = new JTabbedPane();
+	
+	// main split pane
+	private JSplitPane mainContainer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, staffSummary, rightTabPane);
+	
+	public ProjectPanel() {
+		
+		rightTabs.addTab("Summary of Projects", new JPanel());
+		rightTabs.addTab("Project Allocation", new JPanel());
+		rightTabs.setPreferredSize(new Dimension(600, 600));
+		
+		setLayout(new BorderLayout());
+		add(mainContainer, BorderLayout.CENTER);
+	}
+	
 }
