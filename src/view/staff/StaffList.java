6e2df39136bb3df7edfc4afb30164bb216c4662b
rxj01u <rxj01u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-02-01 12:58:27 +0000

    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@84 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/view/staff/StaffList.java
+++ src/view/staff/StaffList.java
@@ -1,12 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 
 public class StaffList extends JPanel {
 
@@ -17,6 +19,8 @@ public class StaffList extends JPanel {
 	private JButton btnAdd = new JButton("Add Staff");
 
 	private JTree tree;
+	
+	DefaultMutableTreeNode staff;
 
 	public StaffList() {
 
@@ -31,38 +35,74 @@ public class StaffList extends JPanel {
 		add(buttonPanel, BorderLayout.SOUTH);
 		setPreferredSize(new Dimension(300, 600));
 
-		DefaultMutableTreeNode top = new DefaultMutableTreeNode(
-				"The Java Series");
-		createNodes(top);
-		tree = new JTree(top);
+		staff = new DefaultMutableTreeNode(
+				"Staff");
+		createNodes();
+		tree = new JTree(staff);
 		JScrollPane treeView = new JScrollPane(tree);
 		setVisible(true);
-		
+				
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("test");
+		test.add("id");
+		addStaff(test);
 		add(tree, BorderLayout.CENTER);
-		
+
+		setVisible(true);
+
 	}
 
-	private void createNodes(DefaultMutableTreeNode top) {
+	private void createNodes() {
 
-		DefaultMutableTreeNode category = null;
-		DefaultMutableTreeNode book = null;
+		DefaultMutableTreeNode name = null;
+		DefaultMutableTreeNode info = null;
 
-		category = new DefaultMutableTreeNode("Books for Java Programmers");
-		top.add(category);
+		name = new DefaultMutableTreeNode("Dominic Chim");
+		staff.add(name);
+		
+		info = new DefaultMutableTreeNode("ID: 000001");
+		name.add(info);
+		
+		info = new DefaultMutableTreeNode("Availability");
+		name.add(info);
 
-		// original Tutorial
-		book = new DefaultMutableTreeNode(
-				"The Java Tutorial: A Short Course on the Basics");
-		category.add(book);
+		name = new DefaultMutableTreeNode(
+				"Ross Jarvis");
+		staff.add(name);
+		
+		info = new DefaultMutableTreeNode("ID: 000002");
+		name.add(info);
+		
+		info = new DefaultMutableTreeNode("Availability");
+		name.add(info);
 	}
 
 	// Add a staff to Staff Summary
-	public void addProject() {
+	public void addStaff(ArrayList<String> staffInfo) {
 
+		DefaultMutableTreeNode name = null;
+		DefaultMutableTreeNode info = null;
+		
+		name = new DefaultMutableTreeNode(staffInfo.get(0));
+//		insertNodeInto
+		staff.add(name);
+				
+		for(String staffDetails : staffInfo) {
+			
+			info = new DefaultMutableTreeNode(staffDetails);
+			name.add(info);
+			
+		}
+		
+//        tree.scrollPathToVisible(new TreePath(s.getPath()));
+
+
+		System.out.println(staff.getChildCount());
+		
 	}
 
 	// Add a task to a project
-	public void addTaskToProject() {
+	public void addStaffInfo() {
 
 	}
 
