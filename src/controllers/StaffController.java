2bdbe03e282b25be1ee3e6b61f90349eee0e612a
rxj01u <rxj01u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-02-02 16:42:19 +0000

    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@94 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

---
+++ src/controllers/StaffController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.staff.MainFrameTest;
+
+public class StaffController implements ActionListener {
+	
+	StaffView view;
+
+	public StaffController(StaffView view) {
+		
+		this.view = view;
+		view.addController(this);
+		
+	}
+
+	public void actionPerformed(ActionEvent ae) {
+
+		String cmd = ae.getActionCommand();
+		
+		switch (cmd) {
+
+		case "Delete Staff":
+			
+			System.out.println("Delete Button");
+			
+			break;
+
+		case "Modify Staff":
+			System.out.println("Modify Button");
+
+			break;
+
+		case "Add Staff":
+			System.out.println("Add Button");
+
+			view.addStaff();
+			
+			break;
+
+		default:
+
+		}
+
+	}
+
+}
