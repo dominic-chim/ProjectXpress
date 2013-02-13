e9e96530ab9f01c7e62a02d0f3d67c662bc3a7b7
kxc12u <kxc12u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-02-13 14:52:10 +0000

    add function to datetime
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@143 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/util/DateTime.java
+++ src/util/DateTime.java
@@ -1,5 +1,25 @@
 package util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTime {
+
+    private String dateTime = "0000-00-00 00:00:00";
+    private final SimpleDateFormat dataBaseDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public DateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public DateTime(int year, int month, int day, int hour, int minute, int second) {
+        // TODO throw some exception when these values are invalid
+        dateTime = String.format("%4d-%2d-%2d %2d:%2d:%2d", 
+                                year, month, day, hour, minute, second);
+    }
+
+    public DateTime(Date date) {
+    }
+
 
 }
