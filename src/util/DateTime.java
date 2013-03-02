3b8bdeb0f9c765c2c6d369d12604d140621447ff
kxc12u <kxc12u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-03-02 14:19:50 +0000

    change it to use int to store datetime
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@183 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/util/DateTime.java
+++ src/util/DateTime.java
@@ -9,21 +9,33 @@ import org.joda.time.Days;
 
 public class DateTime {
 
-	private int year;
-    private String dateTime = "0000-00-00 00:00:00";
+    private int year, month, day, hour, minute, second;
+
+    //private String dateTime = "0000-00-00 00:00:00";
     private final SimpleDateFormat dataBaseDateFormat = new SimpleDateFormat(
             "yyyy-MM-dd HH:mm:ss");
 
     public DateTime(String dateTime) {
-        this.dateTime = dateTime;
+        year = Integer.parseInt(dateTime.substring(0, 4));
+        month = Integer.parseInt(dateTime.substring(5, 7));
+        day = Integer.parseInt(dateTime.substring(8, 10));
+        hour = Integer.parseInt(dateTime.substring(11, 13));
+        minute = Integer.parseInt(dateTime.substring(14, 16));
+        second = Integer.parseInt(dateTime.substring(0, 4));
     }
 
-    public DateTime(int year, int month, int day, int hour, int minute,
-            int second) {
+    public DateTime(int year, int month, int day, 
+            int hour, int minute, int second) {
         // TODO throw some exception when these values are invalid
         //
-        dateTime = String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month,
-                day, hour, minute, second);
+        //dateTime = String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month,
+        //        day, hour, minute, second);
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = minute;
     }
 
     public DateTime(Date date) {
@@ -34,12 +46,16 @@ public class DateTime {
     }
 
     public String getDateTime() {
-        return this.dateTime;
+        String dateTime = String.format("%04d-%02d-%02d %02d:%02d:%02d", 
+                year, month, day, hour, minute, second);
+        return dateTime;
     }
 
 
     public boolean before(DateTime toCompare) {
         try {
+            String dateTime = String.format("%04d-%02d-%02d %02d:%02d:%02d", 
+                                        year, month, day, hour, minute, second);
             Date thisDate = dataBaseDateFormat.parse(dateTime);
             Date dateToCompare = dataBaseDateFormat.parse(toCompare.getDateTime());
             return thisDate.before(dateToCompare);
@@ -52,19 +68,9 @@ public class DateTime {
 
     public static int duration(DateTime start, DateTime end) {
 
-        /*
-        ReadableInstant startRI = (ReadableInstant) start;
-        ReadableInstant endRI = (ReadableInstant) end;
-
-        Days d = Days.daysBetween(startRI, endRI);
-        int days = d.getDays();
-         */
-
         String strStart = start.getDateTime();
         String strEnd = end.getDateTime();
 
-        //int startDay = Integer.parseInt(strStart.substring(8,10));
-        //int endDay = Integer.parseInt(strEnd.substring(8,10));
         DateMidnight sd = new DateMidnight(strStart.substring(0, 10));
         DateMidnight ed = new DateMidnight(strEnd.substring(0, 10));
 
@@ -74,7 +80,30 @@ public class DateTime {
         int endHour = Integer.parseInt(strEnd.substring(11,13));
 
         return (endHour - startHour) + 8 * days;
-    }  
+    }
 
+    // getters
+    public int getYear() {
+        return year;
+    }
 
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
 }
