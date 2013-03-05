24d79b77541e94dcf8f0b0232f0085e9bb6d7031
kxc12u <kxc12u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-03-05 15:29:26 +0000

    init some date
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@198 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

---
+++ database/init.sql
@@ -0,0 +1,6 @@
+insert into skill (skill_id, skill_name) values (1, 'skill_1');
+insert into skill (skill_id, skill_name) values (2, 'skill_2');
+insert into skill (skill_id, skill_name) values (3, 'skill_3');
+
+
+insert into risk_level (task_risk_level, risk_percentage) values ('low', 110);
