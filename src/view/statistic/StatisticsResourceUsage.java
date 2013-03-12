bc3e86f985b05dbf1bb375dd1c7f9b379f7014a7
snd01u <snd01u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-03-12 06:25:06 +0000

    Statistics Panel is complete with the exception of the last chart within the productivity tab.
    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@243 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/view/statistic/StatisticsResourceUsage.java
+++ src/view/statistic/StatisticsResourceUsage.java
@@ -43,20 +43,17 @@ public class StatisticsResourceUsage extends ApplicationFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 5064688526261690032L;
-	public JPanel panel = new JPanel(new GridLayout(2, 1));
-	public JPanel panel2 = new JPanel(new GridLayout(1, 2));
+	public JPanel panel = new JPanel(new GridLayout(1, 2));
+	public JPanel panel2 = new JPanel(new GridLayout(2, 1));
 	public JPanel panel3 = new JPanel();
 
 	JTable table;
 	final StatisticsDao stats = new StatisticsDao();
 
-	final CategoryDataset usageData = totalUsageDataset();
-	final JFreeChart usageChart = totalUsageChart(usageData);
-	final ChartPanel chartPanel = new ChartPanel(usageChart);
 
-	final CategoryDataset mUsageData = multipleUsageDataset();
-	final JFreeChart mUsageChart = multipleUsageChart(mUsageData);
-	final ChartPanel mUsagePanel = new ChartPanel(mUsageChart);
+	final CategoryDataset useddata = usedDataset();
+	final JFreeChart usedchart = usedBarChart(useddata);
+	final ChartPanel usedPanel = new ChartPanel(usedchart);
 
 	final CategoryDataset dataset = createDataset();
 	final JFreeChart chart = taskBarChart(dataset);
@@ -93,11 +90,7 @@ public class StatisticsResourceUsage extends ApplicationFrame {
 		topBorder = BorderFactory
 				.createTitledBorder("Total Resource Usage Per Day");
 		topBorder.setTitlePosition(TitledBorder.TOP);
-		chartPanel.setBorder(topBorder);
-		topBorder = BorderFactory
-				.createTitledBorder("Total Resource Usage Per Day (Per Staff)");
-		mUsagePanel.setBorder(topBorder);
-		topBorder = BorderFactory.createTitledBorder("Staff Usage");
+
 		spTable.setBorder(topBorder);
 		topBorder = BorderFactory.createTitledBorder("Task Count By Staff");
 		taskPanel.setBorder(topBorder);
@@ -105,107 +98,49 @@ public class StatisticsResourceUsage extends ApplicationFrame {
 				.createTitledBorder("Staff Distribution By Project");
 		piePanel.setBorder(topBorder);
 
-		usage.addTab("Total Usage", chartPanel);
-		usage.addTab("Multiple Staff Usage", mUsagePanel);
-		usage.addTab("Staff Usage", spTable);
 
-		chartPanel.setPreferredSize(new Dimension(850, 200));
-		mUsagePanel.setPreferredSize(new Dimension(850, 200));
-		spTable.setPreferredSize(new Dimension(850, 200));
+		usage.addTab("Staff Usage", taskPanel);
+		usage.addTab("Time Usage For Tasks By Staff", usedPanel);
+
+
+		spTable.setPreferredSize(new Dimension(10, 10));
+		piePanel.setPreferredSize(new Dimension(250, 250));
+		usage.setPreferredSize(new Dimension(400, 560));
 
 		panel3.add(usage);
 		panel2.add(piePanel);
-		panel2.add(taskPanel);
+		panel2.add(spTable);
 
-		chartPanel.setPreferredSize(new Dimension(800, 250));
-		mUsagePanel.setPreferredSize(new Dimension(100, 100));
 
 		panel.setPreferredSize(new Dimension(100, 100));
-		panel.add(panel3);
 		panel.add(panel2);
+		panel.add(panel3);
 	}
 
-	private CategoryDataset totalUsageDataset() {
 
-		final String Staff = "Samy Driss";
 
-		final String Day1 = "Day 1";
-		final String Day2 = "Day 2";
-		final String Day3 = "Day 3";
-		final String Day4 = "Day 4";
-		final String Day5 = "Day 5";
-		final String Day6 = "Day 6";
-		final String Day7 = "Day 7";
-		final String Day8 = "Day 8";
-
-		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-		dataset.addValue(1.0, Staff, Day1);
-		dataset.addValue(4.0, Staff, Day2);
-		dataset.addValue(3.0, Staff, Day3);
-		dataset.addValue(5.0, Staff, Day4);
-		dataset.addValue(5.0, Staff, Day5);
-		dataset.addValue(7.0, Staff, Day6);
-		dataset.addValue(7.0, Staff, Day7);
-		dataset.addValue(8.0, Staff, Day8);
-
-		return dataset;
-
-	}
-
-	private CategoryDataset multipleUsageDataset() {
-
-		final String Staff = "Samy Driss";
-		final String Staff2 = "Bob Chen";
-		final String Staff3 = "Ross Jarvis";
-
-		final String Day1 = "Day 1";
-		final String Day2 = "Day 2";
-		final String Day3 = "Day 3";
-		final String Day4 = "Day 4";
-		final String Day5 = "Day 5";
-		final String Day6 = "Day 6";
-		final String Day7 = "Day 7";
-		final String Day8 = "Day 8";
-
-		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-		dataset.addValue(1.0, Staff, Day1);
-		dataset.addValue(4.0, Staff, Day2);
-		dataset.addValue(3.0, Staff, Day3);
-		dataset.addValue(5.0, Staff, Day4);
-		dataset.addValue(5.0, Staff, Day5);
-		dataset.addValue(7.0, Staff, Day6);
-		dataset.addValue(7.0, Staff, Day7);
-		dataset.addValue(8.0, Staff, Day8);
-
-		dataset.addValue(5.0, Staff2, Day1);
-		dataset.addValue(7.0, Staff2, Day2);
-		dataset.addValue(6.0, Staff2, Day3);
-		dataset.addValue(8.0, Staff2, Day4);
-		dataset.addValue(4.0, Staff2, Day5);
-		dataset.addValue(4.0, Staff2, Day6);
-		dataset.addValue(2.0, Staff2, Day7);
-		dataset.addValue(1.0, Staff2, Day8);
-
-		dataset.addValue(4.0, Staff3, Day1);
-		dataset.addValue(3.0, Staff3, Day2);
-		dataset.addValue(2.0, Staff3, Day3);
-		dataset.addValue(3.0, Staff3, Day4);
-		dataset.addValue(6.0, Staff3, Day5);
-		dataset.addValue(3.0, Staff3, Day6);
-		dataset.addValue(4.0, Staff3, Day7);
-		dataset.addValue(3.0, Staff3, Day8);
-
-		return dataset;
-
-	}
 
 	private CategoryDataset createDataset() {
 
 		String staffName = "Staff Name";
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 		ArrayList<Object> statsData = stats.taskCountStaff();
+		for (int i = 0; i < statsData.size(); i++) {
+			ArrayList<Object> row = (ArrayList<Object>) statsData.get(i);
+			dataset.addValue((Number) row.get(1), staffName, row.get(0)
+					.toString());
+		}
+
+		// SELECT skill_name, COUNT(*) AS Total FROM staff_skill_level NATURAL
+		// JOIN skill GROUP BY skill_name;
+		return dataset;
+	}
+	
+	private CategoryDataset usedDataset() {
+
+		String staffName = "Staff Name";
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		ArrayList<Object> statsData = stats.usedData();
 		for (int i = 0; i < statsData.size(); i++) {
 			ArrayList<Object> row = (ArrayList<Object>) statsData.get(i);
 			dataset.addValue((Number) row.get(1), staffName, row.get(0)
@@ -231,43 +166,7 @@ public class StatisticsResourceUsage extends ApplicationFrame {
 		return dataset;
 	}
 
-	private JFreeChart totalUsageChart(final CategoryDataset dataset) {
 
-		final JFreeChart chart = ChartFactory.createLineChart(null, "Day",
-				"Staff", dataset, PlotOrientation.VERTICAL, true, true, false);
-
-		chart.setBackgroundPaint(Color.white);
-
-		final CategoryPlot plot = (CategoryPlot) chart.getPlot();
-		plot.setBackgroundPaint(Color.lightGray);
-		plot.setRangeGridlinePaint(Color.white);
-		plot.setNoDataMessage("No Data Available");
-
-		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-		rangeAxis.setAutoRangeIncludesZero(true);
-
-		return chart;
-	}
-
-	private JFreeChart multipleUsageChart(final CategoryDataset dataset) {
-
-		final JFreeChart chart = ChartFactory.createLineChart(null, "Day",
-				"Staff", dataset, PlotOrientation.VERTICAL, true, true, false);
-
-		chart.setBackgroundPaint(Color.white);
-
-		final CategoryPlot plot = (CategoryPlot) chart.getPlot();
-		plot.setBackgroundPaint(Color.lightGray);
-		plot.setRangeGridlinePaint(Color.white);
-		plot.setNoDataMessage("No Data Available");
-
-		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-		rangeAxis.setAutoRangeIncludesZero(true);
-
-		return chart;
-	}
 
 	private static JFreeChart createPieChart(PieDataset dataset) {
 
@@ -311,6 +210,37 @@ public class StatisticsResourceUsage extends ApplicationFrame {
 		renderer.setDrawBarOutline(false);
 		renderer.setBarPainter(new StandardBarPainter());
 		renderer.setSeriesPaint(0, new Color(78, 130, 190));
+		renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+		renderer.setBaseItemLabelsVisible(true);
+		renderer.setShadowVisible(false);
+		chart.getCategoryPlot().setRenderer(renderer);
+
+		return chart;
+
+	}
+	
+	
+	private static JFreeChart usedBarChart(CategoryDataset dataset) {
+
+		JFreeChart chart = ChartFactory.createBarChart(null,
+
+		"Staff Name", "Number of Tasks", dataset, PlotOrientation.VERTICAL,
+				true, true, false);
+
+		CategoryPlot plot = chart.getCategoryPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setDomainGridlinesVisible(true);
+		plot.setRangeGridlinePaint(Color.white);
+		plot.setNoDataMessage("No Data Available");
+
+		CategoryAxis domainAxis = ((CategoryPlot) plot).getDomainAxis();
+		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+
+		StackedBarRenderer renderer = new StackedBarRenderer(false);
+		renderer.setDrawBarOutline(false);
+		renderer.setBarPainter(new StandardBarPainter());
+		renderer.setSeriesPaint(0, new Color(252, 192, 6));
 		renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
 		renderer.setBaseItemLabelsVisible(true);
 		renderer.setShadowVisible(false);
