cda86bb1990b81df04080511aab2b0e29ef6c399
rxj01u <rxj01u@9ae4bf66-13a9-11e2-a8d0-1fa00df3e515>
2013-03-18 17:31:17 +0000

    git-svn-id: https://code.cs.nott.ac.uk/svn/gp12-mwm/trunk@276 9ae4bf66-13a9-11e2-a8d0-1fa00df3e515

--- src/view/menu/UserManualDialog.java
+++ src/view/menu/UserManualDialog.java
@@ -15,39 +15,86 @@ import javax.swing.JTabbedPane;
 
 public class UserManualDialog extends JDialog {
 
+	//Containers
 	private JPanel jpnlTopContainer = new JPanel();
 	private JPanel jpnlCenterContainer = new JPanel();
 	private JPanel jpnlBottomContainer = new JPanel();
-	private ProjectPanel projectpnl = new ProjectPanel();
 	
+	//Main Tab Panels
+	private JPanel jpnlProject = new ProjectPanel();
+	private JPanel jpnlStaff = new ProjectPanel();
+	private JPanel jpnlStatReports= new ProjectPanel();
+	private JPanel jpnlScheduling = new ProjectPanel();
+	private JPanel jpnlSkills = new ProjectPanel();
+
+	//Buttons
     private JButton jbtnCancel = new JButton("Cancel");
     
-    private JTabbedPane jtbpTabPane = new JTabbedPane();
+    //Tab Pane
+    private JTabbedPane jtbpMainTabPane = new JTabbedPane();
     
+    
+    //Constructor
 	public UserManualDialog(JFrame parent) {
 		
 		super(parent, true);
-		
 		setLayout(new BorderLayout());
-
-		jtbpTabPane.addTab("How to get started", projectpnl);
-
-		jpnlTopContainer.add(jtbpTabPane);
 		
+		//Create Panels for each Tab
+		createStaffPanel();
+		createProjectPanel();
+		createStatReportsPanel();
+		createSchedulingPanel();
+		createSkillsPanel();
+
+		//Add Panels to Tab Pane
+		jtbpMainTabPane.addTab("Project", jpnlProject);
+		jtbpMainTabPane.addTab("Staff", jpnlStaff);
+		jtbpMainTabPane.addTab("Statisical Reports", jpnlStatReports);
+		jtbpMainTabPane.addTab("Skills", jpnlSkills);
+		jtbpMainTabPane.addTab("Schedule Projects", jpnlScheduling);
+
+		//Add Tab Pane and Buttons to Containers
+		jpnlTopContainer.add(jtbpMainTabPane);
 		jpnlCenterContainer.add(jbtnCancel);
-		
-		
+
 		// container settings
 		// jpnlCenterContainer.setPreferredSize(new Dimension(300, 50));
 		add(jpnlTopContainer, BorderLayout.NORTH);
 		add(new JScrollPane(jpnlCenterContainer), BorderLayout.CENTER);
 		add(jpnlBottomContainer, BorderLayout.SOUTH);
+		
 		// dialog settings
 		pack();
 		setTitle("User Manual");
 		setLocationRelativeTo(parent);
 		setVisible(true);
 
+	}
+
+	private void createSkillsPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createSchedulingPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createStatReportsPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createProjectPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createStaffPanel() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public void addControllers(ActionListener controller) {
